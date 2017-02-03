2f424bec1c2fe7c46da4477e35b75dbf2c698355
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2017-02-03 22:19:43 +0000

    clean up

--- src/js/actions/flickrActions.js
+++ src/js/actions/flickrActions.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 export function fetchPublicPhotos(){
 	return (dispatch) => {
 		$.ajax({
-		  url: 'http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=?&tags=Circus',
+		  url: 'http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=?&tags=vw',
 		  dataType: 'jsonp'
 		}).then((data) => {
 			dispatch({type: AT.FETCH_IMAGES, payload: data})
--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPublicPhotos } from '../actions/flickrActions';
 
-
 @connect((store) => {
   return {
     data: store.data,
@@ -23,10 +22,10 @@ export default class Layout extends React.Component {
     const data = this.props;
     for(var k in data.data.items){
       this.photos.push(<div className="col-xs-6"><div className="thumbnail">
-  <div className="caption">Title: {data.data.items[k].title} </div>
-  <div className="border-class"><img className="img-thumbnail" src={data.data.items[k].media.m} /></div><p>tags: {data.data.items[k].tags}</p></div></div>);
+      <div className="caption">Title: {data.data.items[k].title} </div>
+      <div className="border-class"><img className="img-thumbnail" src={data.data.items[k].media.m} /></div><p>tags: {data.data.items[k].tags}</p></div></div>);
     }
-    console.log(this.photos)
+    
     return (
       <div className="container">
         <h1>Flickr latest images</h1>
