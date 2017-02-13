a06a86ac9514d87facebe904239ac84f7ca7ba6d
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2017-02-13 15:41:43 +0000

    add redux devtools extensino

--- src/js/store.js
+++ src/js/store.js
@@ -2,8 +2,9 @@ import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import promise from 'redux-promise-middleware';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducer'
 
 const middleware = applyMiddleware(promise(), thunk, logger());
 
-export default createStore(reducer, middleware);
+export default createStore(reducer, composeWithDevTools(middleware));
